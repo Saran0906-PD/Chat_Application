31850d3fc1bcc0bc411dd94a18599eda844a8cbd
Saran0906-PD <Sarangitacc.com>
2026-02-26 17:33:02 +0530

    Fix skeletons folder case and add SidebarSkeleton

--- frontend/src/components/skeletons/SidebarSkeleton.jsx
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -1,12 +1,11 @@
-import { User } from "lucide-react"
+import { User } from "lucide-react";
 
-const Sidebarskeleton = () => {
+const SidebarSkeleton = () => {
 
-  //create 8 skeleton items
   const skeletonContacts = Array(8).fill(null);
+
   return (
-    <aside
-    className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
+    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
 
       {/* Header */}
       <div className="border-b border-base-300 w-full p-5">
@@ -18,23 +17,26 @@ const Sidebarskeleton = () => {
 
       {/* skeleton contacts */}
       <div className="overflow-y-auto w-full py-3">
-        {skeletonContacts.map((_,idx)=>{
+        {skeletonContacts.map((_, idx) => (
           <div key={idx} className="w-full p-3 flex items-center">
-            {/*Avatar Skeleton */}
-            <div className="reletive mx-auto lg:mx-0">
+
+            {/* Avatar Skeleton */}
+            <div className="relative mx-auto lg:mx-0">
               <div className="skeleton size-12"></div>
             </div>
 
             {/* User info skeleton */}
             <div className="hidden lg:block text-left min-w-0 flex-1">
-              <div className="skeleton h-4 w-32 mb-2"/>
-              <div className="skeleton h-3 w-16"/>
+              <div className="skeleton h-4 w-32 mb-2"></div>
+              <div className="skeleton h-3 w-16"></div>
             </div>
-          </div>
-        })}
-      </div>
-    </aside>
-  )
-}
 
-export default Sidebarskeleton
+          </div>
+        ))}
+      </div>
+
+    </aside>
+  );
+};
+
+export default SidebarSkeleton;
